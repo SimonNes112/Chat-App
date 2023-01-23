aa1747d08e5da0108151696489e1b5878e45d9e7
SimonNes112 <164831@student.horizoncollege.nl>
2023-01-23 12:32:38 +0100

    last changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :user_id)
+    params.require(:post).permit(:title, :content, :user_id, :id)
   end
 end
