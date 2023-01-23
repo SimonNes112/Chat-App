dbab925ae0956e3d4466b68abb603d3c4e0b169d
SimonNes112 <164831@student.horizoncollege.nl>
2023-01-23 16:08:16 +0100

    Last changes for translation file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@ class ApplicationController < ActionController::Base
 
   before_action :validate_user
 
+  around_action :set_locale
+
   def set_user
     @_user = User.find_by(id: session[:user_id])
   end
 
   def validate_user
     redirect_to new_session_path unless @_user
+  end
+
+  def set_locale(&action)
+    session[:locale] = params[:locale] if params[:locale].present?
+
+    @_locale = session[:locale] || I18n.default_locale
+
+    I18n.with_locale(@_locale, &action)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,12 +11,15 @@ module ChatApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = :nl
+    config.i18n.available_locales = %i[nl en]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+     config.time_zone = "Amsterdam"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
